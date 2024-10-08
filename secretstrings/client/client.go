1739902e2e00592869b2eddf72a607a7e779b5fb
kaungzhan <kaungzawhan2000@gmail.com>
2024-10-08 15:38:04 +0100

    with solutions

--- secretstrings/client/client.go
+++ secretstrings/client/client.go
@@ -1,16 +1,50 @@
 package main
+
 import (
-//	"net/rpc"
+	"encoding/csv"
+	"os"
+
+	//	"net/rpc"
 	"flag"
-//	"bufio"
-//	"os"
-//	"uk.ac.bris.cs/distributed2/secretstrings/stubs"
+	"net/rpc"
+	"uk.ac.bris.cs/distributed2/secretstrings/stubs"
+
+	//	"bufio"
+	//	"os"
+	//	"uk.ac.bris.cs/distributed2/secretstrings/stubs"
 	"fmt"
 )
 
-func main(){
-	server := flag.String("server","127.0.0.1:8030","IP:port string to connect to as server")
+func check(e error) {
+	if e != nil {
+		panic(e)
+	}
+}
+func questions() []string {
+	f, err := os.Open("wordlist.csv")
+	check(err)
+	reader := csv.NewReader(f)
+	table, err := reader.ReadAll()
+	check(err)
+	var questions []string
+	for _, row := range table {
+		questions = append(questions, row[0])
+	}
+	return questions
+}
+
+func main() {
+	server := flag.String("server", "127.0.0.1:8030", "IP:port string to connect to as server")
 	flag.Parse()
 	fmt.Println("Server: ", *server)
 	//TODO: connect to the RPC server and send the request(s)
+	client, _ := rpc.Dial("tcp", *server)
+	defer client.Close()
+	qs := questions()
+	for _, q := range qs {
+		request := stubs.Request{q}
+		response := new(stubs.Response)
+		client.Call(stubs.PremiumReverseHandler, request, response)
+		fmt.Println("Responded: ", response.Message)
+	}
 }
